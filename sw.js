4d356192aef6fd59f18817ff00cd38df1f959fdb
willappsoficial-glitch <willappsoficial@gmail.com>
2026-03-03 14:12:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// Service Worker Básico para habilitar a instalação PWA
-const CACHE_NAME = 'totem-powerfisio-v1';
+const CACHE_NAME = 'totem-pf-v2';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
@@ -9,6 +8,9 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
+// Responde às requisições e cria um "modo offline" falso para agradar o Chrome
 self.addEventListener('fetch', (event) => {
-    // Permite que os dados passem direto para não interferir na sua busca do Google Sheets
+    event.respondWith(
+        fetch(event.request).catch(() => new Response('Você está offline.'))
+    );
 });

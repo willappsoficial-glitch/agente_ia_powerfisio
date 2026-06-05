520e7c54414d09961ac1277d68b4a1a53f22fa4a
willappsoficial-glitch <willappsoficial@gmail.com>
2026-06-05 15:56:28 -0300

    Update sw-index.js

--- sw-index.js
+++ sw-index.js
@@ -1,1 +1,38 @@
+const CACHE_NAME = 'maxgym-index-v1'; 
+const urlsToCache = [
+  './index.html',
+  './manifest-index.json',
+  './icon-192.png',
+  './icon-512.png',
+  './fundo-login.jpg' // Lembre-se de subir esta imagem para o GitHub também!
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('maxgym-index-') && cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
